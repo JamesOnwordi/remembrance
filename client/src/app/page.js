db441eee5d6348cdcf3b87a64040644d81f236c9
JamesOnwordi <jamesonwordi50@gmail.com>
2024-08-31 14:35:24 -0600

    first stage done. Foundation complete

---
+++ client/src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <img src="/assets/logo.png" alt="" width={50}/>    <h1>Welcome to Rememberance</h1>  
+      <src></src>
+      </main>
+  );
+}
